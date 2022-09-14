674cebc972084fef12173943fe9427184b5ff5a6
Zoey Batton <Hoodiescool@icloud.com>
2022-09-14 11:34:02 -0400

    change

---
+++ Computer_Shopping/computer_shopping copy.py
@@ -0,0 +1,24 @@
+msi_rtxa5000_price = 4199.35
+gigabyte_aero_price = 4295.54
+razer_blade15_price = 3696.99
+asus_zephyrus_m16_price = 1849.79
+
+products = [msi_rtxa5000_price, gigabyte_aero_price, razer_blade15_price, asus_zephyrus_m16_price]
+
+
+
+# def laptop_prices(most_expensive, least_expensive):
+#     print(f"The most expensive laptop amount is: ${most_expensive}")
+#     print(f"The least expensive laptop amoutn is: ${least_expensive}")
+#     msi_price = round(msi_rtxa5000_price)
+#     print(f"The rounded price of the MSI RTX 5000 is ${msi_price}")
+#     gigabyte_price = round(gigabyte_aero_price)
+#     print(f"The rounded price of the Gigabyte Aero is ${gigabyte_price}")
+#     razer_price = round(razer_blade15_price)
+#     print(f"The rounded price of the Razer Blade 15 is ${razer_price}")
+#     asus_price = round(asus_zephyrus_m16_price)
+#     print(f"The rounded price of the Asus Zephyrus is ${asus_price}")
+#     average = (msi_rtxa5000_price + gigabyte_aero_price + razer_blade15_price + asus_zephyrus_m16_price) / 4
+#     print(f"The average price of all computers is: ${average:.2f}")
+
+# laptop_prices(gigabyte_aero_price, asus_zephyrus_m16_price)
