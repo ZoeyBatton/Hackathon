8fbb0757c46d86cf424e73b2507185300111f02a
Zoey Batton <Hoodiescool@icloud.com>
2022-09-19 16:02:08 -0400

    done

--- Computer_Shopping/computer_shopping copy.py
+++ Computer_Shopping/computer_shopping copy.py
@@ -2,9 +2,10 @@ msi_rtxa5000_price = 4199.35
 gigabyte_aero_price = 4295.54
 razer_blade15_price = 3696.99
 asus_zephyrus_m16_price = 1849.79
+Ninja_warrior = 5002.57
 
-products = [msi_rtxa5000_price, gigabyte_aero_price, razer_blade15_price, asus_zephyrus_m16_price]
-names = ["MSI RTXA5000", "Gigabyte Aero", "Razer Blade 15", "ASUS Zephyrus M16"]
+products = [msi_rtxa5000_price, gigabyte_aero_price, razer_blade15_price, asus_zephyrus_m16_price, Ninja_warrior]
+names = ["MSI RTXA5000", "Gigabyte Aero", "Razer Blade 15", "ASUS Zephyrus M16", "Ninja Warrior"]
 most_expensive = max(products)
 least_expensive = min(products)
 print(f"The most expensive laptop is ${most_expensive}")
