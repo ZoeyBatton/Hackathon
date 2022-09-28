21ff4a0bc05e1a8dbb64c80ea1f7f28f29b8258c
Zoey Batton <Hoodiescool@icloud.com>
2022-09-28 13:22:14 -0400

    changes

--- Snowpocalypse.py
+++ Snowpocalypse.py
@@ -1,6 +1,23 @@
+from importlib.resources import contents
 from bs4 import BeautifulSoup
 from requests import get 
+import os 
+os.system('cls')
+
 url = "https://www.localconditions.com/weather-mahoning-county-ohio/oh396/forecast.php"
 
-
 response = get(url)
+
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+weather = soup.find_all('p', 'forecast-panel-label'), 
+day = soup.find_all('h3', 'forecast-panel-title')
+for item, items in weather:
+    print(item.text)
+    if "Snow" in item:
+        print("Yes")
+    
+
+
