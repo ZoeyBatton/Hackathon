6fd777bc56ba6a2aa01c1cdacee29eeb9d6b6a35
Zoey Batton <Hoodiescool@icloud.com>
2022-09-30 15:41:40 -0400

    Update Pennies_from_heaven.py

--- Pennies_from_heaven.py
+++ Pennies_from_heaven.py
@@ -1,1 +1,15 @@
-pennies = float(input("How many pennies ")
+pennies = int(input("How many pennies do you have? "))
+
+import math 
+
+def dollars(pen):
+    pen_remainder = pen % 100
+    pen_dollars = (pen / 100)
+    pen_quarters = (pen_remainder / 25)
+    pen_q_remainder = ()
+    pen_dimes = (pen_quarters / 10)
+    print(math.trunc(pen_remainder))
+    print(math.trunc(pen_dollars))
+    print(math.trunc(pen_quarters))
+    print(math.trunc(pen_dimes))
+dollars(pennies)
