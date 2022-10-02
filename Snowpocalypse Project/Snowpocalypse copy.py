bcfbc1a243419b17c2206662ba5a305ffd517c03
Zoey Batton <Hoodiescool@icloud.com>
2022-10-02 13:30:59 -0400

    change

--- Snowpocalypse Project/Snowpocalypse copy.py
+++ Snowpocalypse Project/Snowpocalypse copy.py
@@ -3,17 +3,27 @@ from bs4 import BeautifulSoup
 from requests import get 
 import os 
 from twilio.rest import Client
+from re import search
 
 os.system('cls')
 
 url = "https://www.localconditions.com/weather-mahoning-county-ohio/oh396/forecast.php"
+# url_2 = 'https://www.localconditions.com/weather-mahoning-county-ohio/oh396/'
+url_2 = "https://www.localconditions.com/weather-mahoning-county-ohio/oh396/forecast.php"
 
 response = get(url)
+response2 = get(url_2)
 
 account_sid = 'AC10e31a7cfcd37aee8370d2bc09468b25'
 account_token = '9f588b3eda127e946a95df97e62ef3da'
 
 soup = BeautifulSoup(response.text, 'html.parser')
+soup2 = BeautifulSoup(response2.text, 'html.parser')
+
+# day2 = soup2.find('h2', 'portlet-title')
+weather2 = soup2.find('h2', 'portlet-title forecast-panel-title')
+# print(day2.text)
+print(weather2.text)
 
 day = soup.find_all('h3', 'forecast-panel-title')
 weather = soup.find_all('p', 'forecast-panel-label')
@@ -25,46 +35,31 @@ new_list = [[day[0].text, weather[0].text], [day[1].text, weather[1].text], [day
 print(new_list)
 def weathers():
     snow_list = []
-    if  new_list[0][1] == "Chance Of Showers":
+    if search('Mostly Sunny', weather2.text):
+        print("Yes")
+        snow_list.append("Today")
+        print(snow_list)
+    if  new_list[0][1] == "Sunny":
         print("Yes")
         snow_list.append(new_list[0][0])
-    if new_list[1][1] == "Chance Of Showers":
+    if new_list[1][1] == "Sunny":
         print("Yes")  
         snow_list.append(new_list[1][0])
-    if new_list[2][1] == "Chance Of Showers":
+    if new_list[2][1] == "Sunny":
         print("Yes")  
         snow_list.append(new_list[2][0])
-    if new_list[3][1] == "Chance Of Showers":
+    if new_list[3][1] == "Sunny":
         print("Yes")
         snow_list.append(new_list[3][0])
+
+    print(snow_list)
     
-    if len(snow_list) >= 1:
-        message = client.messages.create(
-        messaging_service_sid = 'MGdb54e56bf0ab026edbd4181ec8f08a0c',
-        body = f"It will be raining on {(' and '.join(str(x) for x in snow_list))}",
-        to = '+13303014724'
-        )
-        print(message.sid)
-    
-
-        
-weathers()
-
-
-
-
-
-
-
-print(new_list)
-
-
-
-    
-
+    # if len(snow_list) >= 1:
+    #     message = client.messages.create(
+    #     messaging_service_sid = 'MGdb54e56bf0ab026edbd4181ec8f08a0c',
+    #     body = f"It will be raining on {(' and '.join(str(x) for x in snow_list))}",
+    #     to = '+13303014724'
+    #     )
+    #     print(message.sid)
             
-        
-
-    
-
-
+weathers()
