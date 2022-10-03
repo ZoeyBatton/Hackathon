c0f024ed7990ab618287b07126e09caf11da535e
Zoey Batton <Hoodiescool@icloud.com>
2022-10-03 07:56:27 -0400

    changes

--- Snowpocalypse Project/Snowpocalypse copy.py
+++ Snowpocalypse Project/Snowpocalypse copy.py
@@ -8,7 +8,6 @@ from re import search
 os.system('cls')
 
 url = "https://www.localconditions.com/weather-mahoning-county-ohio/oh396/forecast.php"
-# url_2 = 'https://www.localconditions.com/weather-mahoning-county-ohio/oh396/'
 url_2 = "https://www.localconditions.com/weather-mahoning-county-ohio/oh396/forecast.php"
 
 response = get(url)
@@ -20,41 +19,37 @@ account_token = '9f588b3eda127e946a95df97e62ef3da'
 soup = BeautifulSoup(response.text, 'html.parser')
 soup2 = BeautifulSoup(response2.text, 'html.parser')
 
-# day2 = soup2.find('h2', 'portlet-title')
+
 weather2 = soup2.find('h2', 'portlet-title forecast-panel-title')
-# print(day2.text)
+
 print(weather2.text)
 
 day = soup.find_all('h3', 'forecast-panel-title')
 weather = soup.find_all('p', 'forecast-panel-label')
 
-# search_for = ['Sunny']
-
 client = Client(account_sid, account_token)
-# search("Sunny", weather2.text)
 new_list = [[day[0].text, weather[0].text], [day[1].text, weather[1].text], [day[2].text, weather[2].text], [day[3].text, weather[3].text]]
 
 print(new_list)
 def weathers():
     snow_list = []
-    if search("Sunny", weather2.text):
-        print("Yes")
-        snow_list.append("Tomorrow")
-        print(snow_list)
-    if  search("Sunny", new_list[0][1]):
-        print("Yes")
+    if search("Snow", weather2.text):
+        # print("Yes")
+        snow_list.append("Today")
+    if  search("Snow", new_list[0][1]):
+        # print("Yes")
         snow_list.append(new_list[0][0])
-    if search("Sunny", new_list[1][1]):
-        print("Yes")  
+    if search("Snow", new_list[1][1]):
+        # print("Yes")  
         snow_list.append(new_list[1][0])
-    if search("Sunny", new_list[2][1]):
-        print("Yes")  
+    if search("Snow", new_list[2][1]):
+        # print("Yes")  
         snow_list.append(new_list[2][0])
-    if search("Sunny", new_list[3][1]):
-        print("Yes")
+    if search("Snow", new_list[3][1]):
+        # print("Yes")
         snow_list.append(new_list[3][0])
 
-    # print(snow_list)
+    print(snow_list)
     
     # if len(snow_list) >= 1:
     #     message = client.messages.create(
--- Snowpocalypse Project/Snowpocalypse.py
+++ Snowpocalypse Project/Snowpocalypse.py
@@ -1,13 +1,13 @@
-from importlib.resources import contents
 from bs4 import BeautifulSoup
 from requests import get 
 import os 
 from twilio.rest import Client
+from re import search
 
 os.system('cls')
 
 url = "https://www.localconditions.com/weather-mahoning-county-ohio/oh396/forecast.php"
-url_2 = 'https://www.localconditions.com/weather-mahoning-county-ohio/oh396/'
+url_2 = "https://www.localconditions.com/weather-mahoning-county-ohio/oh396/forecast.php"
 
 response = get(url)
 response2 = get(url_2)
@@ -18,34 +18,36 @@ account_token = '9f588b3eda127e946a95df97e62ef3da'
 soup = BeautifulSoup(response.text, 'html.parser')
 soup2 = BeautifulSoup(response2.text, 'html.parser')
 
-day2 = soup2.find('h2', 'portlet-title')
-weather2 = soup2.find('p', style_='font-size:20pt; padding-top:0px !important; margin-top:0px !important;')
-weather2 = soup.select_one('p', style_ ='[style="font-size:20pt; padding-top:0px !important; margin-top:0px !important;"]')
-print(day2.text)
-print(weather2.text)
+weather2 = soup2.find('h2', 'portlet-title forecast-panel-title')
+
+# print(weather2.text)
 
 day = soup.find_all('h3', 'forecast-panel-title')
 weather = soup.find_all('p', 'forecast-panel-label')
 
 client = Client(account_sid, account_token)
-
 new_list = [[day[0].text, weather[0].text], [day[1].text, weather[1].text], [day[2].text, weather[2].text], [day[3].text, weather[3].text]]
 
-print(new_list)
+# print(new_list)
 def weathers():
     snow_list = []
-    if  new_list[0][1] == "Chance Of Showers":
-        print("Yes")
+    if search("Snow", weather2.text):
+        # print("Yes")
+        snow_list.append("Today")
+    if  search("snow", new_list[0][1]):
+        # print("Yes")
         snow_list.append(new_list[0][0])
-    if new_list[1][1] == "Chance Of Showers":
-        print("Yes")  
+    if search("snow", new_list[1][1]):
+        # print("Yes")  
         snow_list.append(new_list[1][0])
-    if new_list[2][1] == "Chance Of Showers":
-        print("Yes")  
+    if search("snow", new_list[2][1]):
+        # print("Yes")  
         snow_list.append(new_list[2][0])
-    if new_list[3][1] == "Chance Of Showers":
-        print("Yes")
+    if search("snow", new_list[3][1]):
+        # print("Yes")
         snow_list.append(new_list[3][0])
+
+    # print(snow_list)
     
     # if len(snow_list) >= 1:
     #     message = client.messages.create(
